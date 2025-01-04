b3f3e10242db267781b608d5ad0f483485878cf7
Sridhar Kasukurthy <kasukur@gmail.com>
2025-01-04 16:30:47 +1100

    Fix webpack configuration and update dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,29 @@
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  config.resolve.fallback = {
-    "crypto": require.resolve("crypto-browserify"),
-    "path": require.resolve("path-browserify"),
-    "os": require.resolve("os-browserify/browser"),
-    "http": require.resolve("stream-http"),
-    "buffer": require.resolve("buffer/"),
-    "stream": require.resolve("stream-browserify"),
-    "process": require.resolve("process/browser")
+  config.resolve = {
+    ...config.resolve,
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "http": require.resolve("stream-http"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "process": false,
+      "util": require.resolve("util/"),
+      "assert": require.resolve("assert/")
+    }
   };
 
   config.plugins = [
     ...config.plugins,
     new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
     })
   ];
 
